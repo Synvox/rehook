c2b83010aa5cdd976755cf4ea061e578a563092c
Ryan Allred <ryanallredxyz@gmail.com>
2018-11-04 15:01:26 -0700

    Make branch static

--- src/rehook/branch.js
+++ src/rehook/branch.js
@@ -1,5 +1,10 @@
+import React from 'react'
+// @ts-ignore
+const { useMemo } = React
+
 // Note, branching disobeys one of the hook rules because
-// it wraps hooks in a condition.
+// it wraps hooks in a condition. For this reason, the branch
+// is cached and kept the same regardless of updates.
 
 /**
  *
@@ -9,7 +14,9 @@
  * @returns {Function}
  */
 const branch = (condition, left, right = x => x) => props => {
-  return condition(props) ? left(props) : right(props)
+  const conditionResult = useMemo(() => condition(props), [])
+
+  return conditionResult ? left(props) : right(props)
 }
 
 export default branch
